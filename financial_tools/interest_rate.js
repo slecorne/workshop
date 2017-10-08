e2ad44fe5b93f20092a87ba6e0fbbe95d0757f44
stephane <stephane.lecorney@gmail.com>
2017-10-08 14:37:10 +0200

    use moment to allow different csv date format parsing

--- financial_tools/interest_rate.js
+++ financial_tools/interest_rate.js
@@ -1,7 +1,7 @@
 'use strict';
 
 var leapYear = require('leap-year');
-
+var moment = require('moment');
 
 // convention CH/Eu is 360 days with interest
 // https://www.vermoegenszentrum.ch/conseils/articles/particuliers/lever-une-hypotheque/les-methodes-de-decompte-des-interets.html
@@ -9,28 +9,28 @@ var leapYear = require('leap-year');
 // start and end should be date object
 function numDaysWithInterest(start, end) {
 	var days = 0;
-	if (end.getYear()===start.getYear()) {
-		if (end.getMonth()===start.getMonth()) {
+	if (end.year()===start.year()) {
+		if (end.month()===start.month()) {
 			// within a month
-			days = end.getUTCDate()-start.getUTCDate();
+			days = end.date()-start.date();
 			return days;
 		} 
 		// within same year
 		// count 30 days for each full months betweend start and end
-		var numCompleteMonths = (end.getMonth()-start.getMonth()-1);
+		var numCompleteMonths = (end.month()-start.month()-1);
 		days += 30 * numCompleteMonths;
-		var startMonth = 30 - start.getUTCDate();
-		var endMonth = end.getUTCDate();
+		var startMonth = 30 - start.date();
+		var endMonth = end.date();
 		// add days of initial month
 		// handle specific case february
-		if (start.getMonth()!==1 /*getMonth starts at 0*/ ) { 
+		if (start.month()!==1 /*month starts at 0*/ ) { 
 			// normal case
 			days += startMonth>0 ? startMonth : 0;
 		} else {
-			if (leapYear(start.getYear()) && start.getUTCDate()==29) {
+			if (leapYear(start.year()) && start.date()==29) {
 				//last day is 29
 				// no day counted for that month
-			} else if (!leapYear(start.getYear()) && start.getUTCDate()==28)  {
+			} else if (!leapYear(start.year()) && start.date()==28)  {
 				// last day is 28
 				// no day counted for that month
 			} else {
@@ -40,15 +40,15 @@ function numDaysWithInterest(start, end) {
 		}
 		// add day of final month
 		// handle specific case february
-		if (end.getMonth()!==1 /*getMonth starts at 0*/ ) { 
+		if (end.month()!==1 /*month starts at 0*/ ) { 
 			// normal case
 			days += endMonth>30 ? 30 : endMonth;
 		} else {
-			if (leapYear(end.getYear()) && end.getUTCDate()==29) {
+			if (leapYear(end.year()) && end.date()==29) {
 				//last day is 29
 				// no day counted for that month
 				days += 30;
-			} else if (!leapYear(end.getYear()) && end.getUTCDate()==28)  {
+			} else if (!leapYear(end.year()) && end.date()==28)  {
 				// last day is 28
 				// 30 days for that month
 				days += 30;
@@ -60,12 +60,20 @@ function numDaysWithInterest(start, end) {
 
 	} else {
 		// different years
-		days = 360*(end.getYear()-start.getYear());
+		days = 360*(end.year()-start.year());
 	}
 
 	return days;
 }
 
+// get Date object from javascript date format or CSV/xls format (also support FR)
+function dateFromString(date, format) {
+	if (!format) {
+		return moment(date);
+	}
+	// try parsing csv format
+	return moment(date, format);
+}
 
 // data should contain:
 // start: start date
@@ -80,13 +88,14 @@ function interest(data) {
 
 	var YEAR = 360; // 360 commercial days
 
-	var startDate = new Date(data.start);
-	var endDate = new Date(data.end);
+	var startDate = dateFromString(data.start);
+	var endDate = dateFromString(data.end);
 	var numDays = numDaysWithInterest(startDate, endDate);
 	return data.deposit*data.rate*(numDays/YEAR);
 }
 
 module.exports = {
 	interest: interest,
-	numDaysWithInterest: numDaysWithInterest
+	numDaysWithInterest: numDaysWithInterest,
+	dateFromString: dateFromString
 };
--- spec/financial_tools/InterestRateSpec.js
+++ spec/financial_tools/InterestRateSpec.js
@@ -1,41 +1,54 @@
+
+var moment = require('moment');
+
 describe("InterestRate", function() {
   var RateCalculator = require('../../financial_tools/interest_rate');
 
   describe("should compute correctly number of relevant days", function() {
     it("should consider 360 days for a full year", function() {
-      let start = new Date("2016-01-01");
-      let end = new Date("2017-01-01");
+      let start = moment("2016-01-01");
+      let end = moment("2017-01-01");
       expect(RateCalculator.numDaysWithInterest(start, end)).toEqual(360);
     });
 
     it("should consider correctly 31 days month as 30 commercial days", function() {
-      let start = new Date("2017-06-10");
-      let end = new Date("2017-08-31");
+      let start = moment("2017-06-10");
+      let end = moment("2017-08-31");
       expect(RateCalculator.numDaysWithInterest(start, end)).toEqual(80);
     });
 
     it("should consider february correctly for non leap year (start)", function() {
-      let start = new Date("2017-02-28");
-      let end = new Date("2017-04-11");
+      let start = moment("2017-02-28");
+      let end = moment("2017-04-11");
       expect(RateCalculator.numDaysWithInterest(start, end)).toEqual(41);
     });
 
     it("should consider february correctly for non leap year (end)", function() {
-      let start = new Date("2017-01-11");
-      let end = new Date("2017-02-28");
+      let start = moment("2017-01-11");
+      let end = moment("2017-02-28");
       expect(RateCalculator.numDaysWithInterest(start, end)).toEqual(49);
     });
 
     it("should consider february correctly for leap year (start)", function() {
-      let start = new Date("2016-02-28");
-      let end = new Date("2016-04-11");
+      let start = moment("2016-02-28");
+      let end = moment("2016-04-11");
       expect(RateCalculator.numDaysWithInterest(start, end)).toEqual(43);
     });
 
     it("should consider february correctly for non leap year (end)", function() {
-      let start = new Date("2016-01-11");
-      let end = new Date("2016-02-28");
+      let start = moment("2016-01-11");
+      let end = moment("2016-02-28");
       expect(RateCalculator.numDaysWithInterest(start, end)).toEqual(47);
+    });
+
+  });
+
+  describe("should parse correctly dates", function() {
+    it("should accept csv format US", function() {
+      var date = RateCalculator.dateFromString("12/09/2017","DD/MM/YYYY");
+      expect(date.year()).toEqual(2017);
+      expect(date.month()).toEqual(8); // month -1 
+      expect(date.date()).toEqual(12);
     });
 
   });
